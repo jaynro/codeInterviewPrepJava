d0f1a3cfcca8d4d6f99cf44de4ffa368556397b1
jaynro <jaynro@gmail.com>
2022-09-10 17:06:03 -0500

    Create validateSubsequence.java
    first draft

---
+++ validateSubsequence.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Program {
+  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    System.out.println("isValidSubsequence: start: ");
+
+  }
+}
