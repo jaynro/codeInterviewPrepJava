11c811b10c149e975d345003459d4646065240c6
jaynro <jaynro@gmail.com>
2022-09-10 17:27:06 -0500

    Update validateSubsequence.java
    first draft

--- validateSubsequence.java
+++ validateSubsequence.java
@@ -2,7 +2,59 @@ import java.util.*;
 
 class Program {
   public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-    System.out.println("isValidSubsequence: start: ");
 
+    
+    // SequencePoniter=0
+    int j =0;
+    
+    //iterate sequence
+    for(int i=0; i<array.length(); i++){
+      if (seq[i]==seq[j])
+        j++;
+  
+      if (j ==sequence.lenhgt() -1)
+        return true;
+        
+    }
+
+
+    //no records found    
+    return false;
   }
 }
+
+
+/*
+option1
+array=[5, 1, 22, 25, 6, -1, 8, 10]
+sequence =[1, 6, -1, 10]
+true
+
+i  a[i]        [1,      6,      -1,     10]
+    [
+0   5,          -   
+1   1,          ok     
+2   22,                -
+3   25,                - 
+4   6,                 ok
+5   -1,                         ok
+5   8,                          -
+6   10                                  ok - exit
+   ]
+
+j// SequencePoniter=0
+
+iterate sequence
+  if seq[i]==seq[j]
+    j++
+  
+  if j ==sequence.lenhgt[] -1
+    return true
+
+return false
+
+
+
+
+
+*/
