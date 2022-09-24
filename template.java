c3ca7d5df906ae5ab24e2e55bf2d1a473bcd7343
jaynro <jaynro@gmail.com>
2022-09-24 14:22:58 -0500

    Create template.java

---
+++ template.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+
+        System.out.println("case : " +  func(  )); 
+    }
+    
+    public static int func( ){
+        System.out.println("func "  ); 
+        return -1;        
+
+    }
+}
